68ec653981e88197be44fca42c65f19d6a1a02b8
Mario <mgcs1161@gmail.com>
2024-09-27 11:14:13 +0100

    Simplify Input

--- genres.py
+++ genres.py
@@ -4,14 +4,24 @@ from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import GridSearchCV
+import string
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
-
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
 # -----------------------------------------------------------------------------
 #                                  Config
 # -----------------------------------------------------------------------------
 
 # If the model trains with only the first 50 words of the plot
-weak = True
+weak = False
 
 trainPath = "./train.txt"
 trainDF = pd.read_csv(trainPath, sep='\t', names=['title', 'from', 'genre', 'director', 'plot'])
@@ -22,6 +32,67 @@ testDF = pd.read_csv(testPath, sep='\t', header=None, names=['title', 'from', 'd
 # -----------------------------------------------------------------------------
 #                                 Main Code
 # -----------------------------------------------------------------------------
+
+def apply_preprocessing(text):
+    contractions = {
+        "ain't": "am not",
+        "aren't": "are not",
+        "can't": "cannot",
+        "could've": "could have",
+        "couldn't": "could not",
+        "didn't": "did not",
+        "doesn't": "does not",
+        "don't": "do not",
+        "hadn't": "had not",
+        "hasn't": "has not",
+        "haven't": "have not",
+        "he's": "he is",
+        "i'd": "i would",
+        "i'll": "i will",
+        "i'm": "i am",
+        "i've": "i have",
+        "isn't": "is not",
+        "it's": "it is",
+        "let's": "let us",
+        "mustn't": "must not",
+        "shan't": "shall not",
+        "she's": "she is",
+        "should've": "should have",
+        "shouldn't": "should not",
+        "that's": "that is",
+        "there's": "there is",
+        "they're": "they are",
+        "wasn't": "was not",
+        "we'd": "we would",
+        "we're": "we are",
+        "weren't": "were not",
+        "what's": "what is",
+        "who's": "who is",
+        "won't": "will not",
+        "wouldn't": "would not",
+        "would've" : "would have",
+        "you'd": "you would",
+        "you're": "you are",
+    }
+    lowered = text.lower()
+    lowered_re = re.sub(r'[^a-zA-Z\s]', '', lowered)
+    words = nltk.word_tokenize(lowered_re)
+    
+    preproc = ' '.join(contractions.get(word, word) for word in words)
+    tokens = word_tokenize(preproc, "english")
+    
+    for token in tokens:
+        if(all(char in string.punctuation for char in token)):
+            tokens.remove(token)
+
+    stop_words = set(stopwords.words('english'))
+    filtered_tokens = [word for word in tokens if word not in stop_words]
+
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
+    
+    return ' '.join(lemmatized_tokens)
+
 
 X_train = None
 if not weak:
@@ -37,7 +108,8 @@ y_train = trainDF['genre']
 X_test = testDF['plot']
 
 # Maybe something to process the plot ????
-
+X_train = X_train.apply(apply_preprocessing)
+X_test = X_test.apply(apply_preprocessing)
 
 # Naive Bayes pipeline with CountVectorizer and TF-IDF
 nb_pipeline = Pipeline([
